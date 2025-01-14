0d3724eeac5d598d8958dc3161e0d09474f2faad
Akiliyh <akiliyhsensei@gmail.com>
2025-01-14 14:22:02 +0100

    added fullscreen

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-const colors = ["#EE6B49","#FEDDCF", "#D7E5F4", "#77B5FE", "#0131B4", "#48392A"];
-const gridColors = [[]];
+var w = window.innerWidth;
+var h = window.innerHeight;  
+const COLORS = ["#EE6B49","#FEDDCF", "#D7E5F4", "#77B5FE", "#0131B4", "#48392A"];
+const GRIDCOLORS = [[]];
 let curColorIndex = 0;
 let curColor = 0;
 let coords = [0,0,0];
 let angle = [];
 let size = [];
 let speed = [];
+let canvas = 0;
+const RANGE = 200;
 
 function setup() {
-  let cnv = createCanvas(800, 800, WEBGL);
+  canvas = createCanvas(w, h, WEBGL);
   colorMode(HSL);
   noStroke();
-  for (let i = 1; i < 5; i++) {
-    let curColorIndex = Math.round(random(0,colors.length-1));
-    gridColors[i] = colors[curColorIndex];
-    coords[i] = [random(-50,50),random(-50,50),random(-50,50)];
-    angle[i] = random(-50,50);
+  for (let i = 1; i < 4; i++) {
+    let curColorIndex = Math.round(random(0,COLORS.length-1));
+    GRIDCOLORS[i] = COLORS[curColorIndex];
+    COLORS.splice(curColorIndex, 1);
+    coords[i] = [random(-RANGE,RANGE),random(-RANGE,RANGE),random(-RANGE,RANGE)];
+    angle[i] = random(-RANGE,RANGE);
     size[i] = random(20,50);
     speed[i] = random(0, 0.01);
   }
-    setTimeout(() => {
-    save(cnv, 'myCanvas.png');
-    }, 1000);
 }
 
 function draw() {
-background(100);
-  orbitControl();
-  for (let i = 1; i < 5; i++) { 
+background(COLORS[COLORS.length-1]);
+  orbitControl(.2,.2,.2);
+  for (let i = 1; i < 4; i++) { 
       push();
-      fill(gridColors[i]);
+      fill(GRIDCOLORS[i]);
       //square(100*column, 100*line, 100);
       translate(coords[i][0], coords[i][1], coords[i][2]);
       translate(-0,0);
@@ -37,10 +39,17 @@ background(100);
       //rotateY(frameCount * 0.01+line);
       rotateX(frameCount * speed[i]+angle[i]);
       rotateY(frameCount * speed[i]+angle[i]);
-      //cone(30);
-      plane(size[i]);
+      //torus(80,15,80,80);
+      ellipsoid(size[i]*5, 200, 60, 100, 100);
       pop();     
       //rotateY(frameCount * 0.01);
       //translate(-50,-50,0);
   } 
+}
+
+window.onresize = function() {
+  // assigns new values for width and height variables
+  w = window.innerWidth;
+  h = window.innerHeight;  
+  canvas.size(w,h);
 }
